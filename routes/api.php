cc803ed54ffb151f31cc5c9d8aba81912048f118
Nikola <nikolaviki@hotmail.com>
2020-03-19 14:55:25 +0100

    Zadatak 2

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('api')->get('/movies', 'MoviesController@index');
+Route::middleware('api')->get('/movies/{id}', 'MoviesController@show');
+Route::middleware('api')->post('/movies', 'MoviesController@store');
+Route::middleware('api')->put('/movies/{id}', 'MoviesController@update');
+Route::middleware('api')->delete('/movies/{id}', 'MoviesController@destroy');
+
+
